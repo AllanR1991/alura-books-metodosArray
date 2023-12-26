57b29e18a3efc6407b34ac0ba4dd71e08b8d3d98
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-12-26 15:35:34 -0300

    Melhorando a legibilidade e inserindo valor total dos livros no rodape

--- app/filter.js
+++ app/filter.js
@@ -4,10 +4,38 @@ botoes.forEach(botao => {
     botao.addEventListener('click', filtrarLivros)
 })
 
-function filtrarLivros(event){
+function filtrarLivros(event) {
+    //debugger
     botaoSelecionado = event.target.value;
     console.log(botaoSelecionado);
-    let livrosFiltrados = botaoSelecionado = 'disponivel'? livros.filter(livro=> livro.quantidade>0) : livros.filter(livro => livro.categoria == botaoSelecionado);
+
+    let livrosFiltrados = (botaoSelecionado == 'disponiveis') ?        
+        filtrarProDisponibilidade() :
+        filtrarPorCategoria() ;
+    
     elementoParaInserirLivros.innerHTML = '';
     exibeLivrosNaTela(livrosFiltrados);
+    if(botaoSelecionado == 'disponiveis'){
+        const valorTotal = calcularValotTotalDeLivrosDisponiveis(livrosFiltrados);
+        exibirValorTotalDosLivrosDisponiveis(valorTotal);
+    }
 }
+
+function filtrarPorCategoria() {
+    return livros.filter(livro => livro.categoria == botaoSelecionado);
+}
+
+function filtrarProDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveis(valorTotal){
+    elementoComValorTotalLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
+
+}
+
+
